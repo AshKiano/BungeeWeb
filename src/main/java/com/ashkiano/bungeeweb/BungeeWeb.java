78445be0d654f5d95920d31b1938224713388cfc
AshKiano <ash.kiano@gmail.com>
2023-07-07 14:37:20 +0200

    version 1.10

--- src/main/java/com/ashkiano/bungeeweb/BungeeWeb.java
+++ src/main/java/com/ashkiano/bungeeweb/BungeeWeb.java
@@ -20,7 +20,7 @@ public class BungeeWeb extends Plugin {
     private Configuration configuration;
 
     // Array of supported languages
-    private String[] languages = {"en", "cs", "sk", "de", "zh_cn"};
+    private String[] languages = {"en", "cs", "sk", "de", "zh_cn", "pl"};
 
     @Override
     public void onEnable() {
